d442178b248d2dcf2c34268ce43a9856c680fe83
rehmet <ryfgmz87@gmail.com>
2024-10-07 15:27:34 +0000

    frontend env

--- frontend/src/Services/Services.tsx
+++ frontend/src/Services/Services.tsx
@@ -1,13 +1,13 @@
 import axios from 'axios' 
 
 const Api = {
-  uploadpdf: data => axios.post('http://0.0.0.0:5900/api/upload', data),
-  audioUpload: data => axios.post('http://0.0.0.0:5900/api/audio_upload',data),
-  sessionCreate: data => axios.post('http://0.0.0.0:5900/api/create_user_session', data),
-  fetchSession: data => axios.post('http://0.0.0.0:5900/wv/fetch_user_session', data),
-  fetchChatHistory: data => axios.post('http://0.0.0.0:5900/wv/fetch_chat_history', data),
-  clarify: data => axios.post('http://0.0.0.0:5900/api/clarify', data),
-  overallmetrics: data => axios.post('http://0.0.0.0:5900/api/calculate_overall_progress', data)
+  uploadpdf: data => axios.post(`${import.meta.env.VITE_REACT_APP_BACKEND_URL}/api/upload`, data),
+  audioUpload: data => axios.post(`${import.meta.env.VITE_REACT_APP_BACKEND_URL}/api/audio_upload`,data),
+  sessionCreate: data => axios.post(`${import.meta.env.VITE_REACT_APP_BACKEND_URL}/api/create_user_session`, data),
+  fetchSession: data => axios.post(`${import.meta.env.VITE_REACT_APP_BACKEND_URL}wv/fetch_user_session`, data),
+  fetchChatHistory: data => axios.post(`${import.meta.env.VITE_REACT_APP_BACKEND_URL}/wv/fetch_chat_history`, data),
+  clarify: data => axios.post(`${import.meta.env.VITE_REACT_APP_BACKEND_URL}/api/clarify`, data),
+  overallmetrics: data => axios.post(`${import.meta.env.VITE_REACT_APP_BACKEND_URL}/api/calculate_overall_progress`, data)
 };
 
 
