eeb6e58951614deeb1e3511353b78ac75ce9da0f
Yabebal Fantaye <yabebal@10academy.org>
2025-05-30 05:32:51 +0000

    changed socketio log to critical

--- api/pages/ipersona/socket/ipersona_socket.py
+++ api/pages/ipersona/socket/ipersona_socket.py
@@ -29,8 +29,8 @@ transcriber = None
 
 sio = socketio.AsyncServer(cors_allowed_origins="*", 
                            async_mode="asgi",
-                           logger=True,
-                           engineio_logger=True)
+                           logger=False,
+                           engineio_logger=False)
 
 
 # socket_app = socketio.ASGIApp(sio)
